e6d165550b54a27c5dff3585c0274e07727cd744
Cr1ms3nRav3n <66265636+Cr1ms3nRav3n@users.noreply.github.com>
2021-08-03 23:14:29 -0500

    Add files via upload

---
+++ vial-firmware/fourier/keymaps/vial/config.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x06, 0x22, 0xE6, 0xDF, 0xA3, 0x67, 0xFF, 0x4F}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 7 }
+
+#define VIAL_UNLOCK_COMBO_COLS { 0, 5 } 
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 0
