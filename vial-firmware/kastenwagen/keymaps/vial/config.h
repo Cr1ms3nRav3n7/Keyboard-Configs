aff9b7eadbd1658fc0afbe21a9e73b5f8747b9bb
Cr1ms3nRav3n <66265636+Cr1ms3nRav3n@users.noreply.github.com>
2021-08-04 10:12:47 -0500

    Add files via upload
    Added files to compile kastenwagwen

---
+++ vial-firmware/kastenwagen/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xCA, 0xCA, 0xE1, 0x5F, 0x5F, 0x32, 0x27, 0x68}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 5 }
+
+#define VIAL_UNLOCK_COMBO_COLS { 0, 5 }
